1b35136c1eb5a827bd02483639d3d084d3832933
alexiskelsall <alexis@LexiK.>
2025-03-03 11:19:43 +0000

    started login page

---
+++ LoginScreen.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+//styling sheet required here? <<<<<< ------- import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+
+export default function LoginScreen(){
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    //error handling state
+    //loading handling state 
+
+
+    //function to handle the login / error 
+
+
+    return (
+        <Text>Hey Buddy, Welcome back</Text>
+    )
+}
