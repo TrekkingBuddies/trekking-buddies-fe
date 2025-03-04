4a4ec60c013d73c2e5b8b24ce81b9b7c1150e8b3
alexiskelsall <alexis@LexiK.>
2025-03-04 16:40:01 +0000

    Hikers-List

--- Components/HikersListComponents/HikersList.js
+++ Components/HikersListComponents/HikersList.js
@@ -7,8 +7,8 @@ export default function HikersList() {
     const [hikers, setHikers] = useState([])
 
     useEffect(() => {
-        getHikers().then((hikers) => {
-            setHikers(hikers)
+        getHikers().then((users) => {
+            setHikers(users)
         })
     }, [])
     return (
