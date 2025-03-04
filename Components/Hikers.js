6a583a5f95a0a1ddb629dad8a5641cb76a950fba
BrandonSeed <brandon@seedshouse.co.uk>
2025-03-04 10:19:04 +0000

    rename file for consistency

---
+++ Components/Hikers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {ScrollView, Text, View} from 'react-native';
+import HikersList from './HikersList Components/HikersList';
+
+export default function Hikers (){
+
+
+
+return (
+    <View>
+        <Text>The filter bar goes here</Text>
+        <ScrollView>
+            <HikersList />
+        </ScrollView>
+    </View>
+)
+
+}
