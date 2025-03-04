5a37712be9edb46f3fa680d01354c4ee80c7d24b
BrandonSeed <brandon@seedshouse.co.uk>
2025-03-04 10:18:40 +0000

    rename file for consistency

---
+++ Components/HikersList Components/HikersList.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function HikersList() {
+
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <View>
+
+        </View>
+    )
+}
