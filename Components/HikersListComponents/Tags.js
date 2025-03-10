2bca74af661bd85e80c9feed5d48a494305edac7
BrandonSeed <brandon@seedshouse.co.uk>
2025-03-10 10:34:35 +0000

    move styles to seperate file

--- Components/HikersListComponents/Tags.js
+++ Components/HikersListComponents/Tags.js
@@ -4,8 +4,8 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
 } from "react-native";
+import styles from "../../styles/tagsStyles";
 
 export default function Tags({ setFilters }) {
   const tags = ["uphill", "flat", "countryside", "dog friendly"];
@@ -47,32 +47,3 @@ export default function Tags({ setFilters }) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  tagContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    marginTop: 6,
-  },
-  tagWrapper: {
-    flexDirection: "row",
-    marginRight: 3,
-    marginBottom: 5,
-  },
-  tag: {
-    borderRadius: 20,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  unselectedTag: {
-    backgroundColor: "#ccc",
-  },
-  selectedTag: {
-    backgroundColor: "#CAD2C5",
-  },
-  tagText: {
-    color: "black",
-    fontWeight: "bold",
-    fontSize: 14,
-  },
-});
