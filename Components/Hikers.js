b05edb4675e35a5c61f901c57c32686aecd06398
BrandonSeed <brandon@seedshouse.co.uk>
2025-03-12 10:29:36 +0000

    remove unused import

--- Components/Hikers.js
+++ Components/Hikers.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import HikersList from "./HikersListComponents/HikersList";
 import HikersFilter from "./HikersListComponents/HikersFilter";
